2fbf943717457bbdfd3beba1d75f1660dbfce4b8
Velocifero1052 <commelevent1052@yandex.ru>
2019-09-26 13:01:47 +0500

    contrellers added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,12 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+    private int currentPage = 1;
+    private String searchUrlTemplate = "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=8c3a08d2ae655faaaa9e19146ec0985d&text={SEARCH_TEXT}&format=json&page=%d";
     @FXML
     private Button searchButton;
 
@@ -14,10 +18,18 @@ public class Controller {
     @FXML
     private Button nextButton;
 
+    @FXML
+    private TextField searchField;
+
 
     @FXML
     private void search(ActionEvent event){
         System.out.println("search button click");
+        String text = searchField.getText();
+
+        System.out.println(text);
+
+
     }
 
     @FXML
---
+++ src/sample/models/Resp.java
@@ -0,0 +1,39 @@
+package sample.models;
+
+
+import org.json.simple.JSONObject;
+
+public class Resp {
+    String status;
+    Photos photos;
+
+    public Resp(JSONObject jsonObject){
+        status = (String)jsonObject.get("stat");
+        photos = new Photos((JSONObject) jsonObject.get("photos"));
+    }
+
+
+    public Photos getPhotos() {
+        return photos;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Resp{" +
+                "status=" + status +
+                ", photos=" + photos +
+                '}';
+    }
+
+    public void setPhotos(Photos photos) {
+        this.photos = photos;
+    }
+}
